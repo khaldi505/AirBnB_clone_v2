62f66edb6143226ae447c45657012c095c3f085d
khaldi505 <habetlbrika69@gmail.com>
2020-08-17 17:55:51 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,17 +3,12 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install nginx
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 content="Holberton school"
 echo "$content" > /data/web_static/releases/test/index.html
-if test -L /data/web_static/current; then
-	rm -rf /data/web_static/current
-	ln -sf /data/web_static/releases/test/ /data/web_static/current
-else
-	ln -sf /data/web_static/releases/test/ /data/web_static/current
-fi;
-sudo chown -hR ubuntu:/data
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu: /data
 sudo sed -i '55i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
