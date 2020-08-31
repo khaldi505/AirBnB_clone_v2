f9e22bc1cdf963df4467b361c9b18243f617f695
khaldi505 <habetlbrika69@gmail.com>
2020-08-31 14:44:00 +0100

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# a script that starts a flask web application
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+app.url_map.strict_slashes = False
+@app.route('/')
+def hello_hbnb():
+	""" returns Hello hbnb!"""
+
+	return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+	""" returns Hello hbnb!"""
+
+	return "HBNB"
+
+if __name__ == '__main__':
+   app.run()
