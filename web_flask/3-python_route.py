74e66d85d1f911ea4429fa1f881b4266da1c7e92
khaldi505 <habetlbrika69@gmail.com>
2020-09-01 19:37:40 +0100

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+a script that starts a flask web application
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ returns Hello hbnb!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns Hello hbnb!"""
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ returns c followed by a text """
+    text = text.replace("_", " ")
+    return 'C %s' % text
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    returns python followed by a text
+    if the input does not exist the default text is "is cool"
+    """
+
+    text = text.replace("_", " ")
+    return 'Python %s' % text
+
+
+if __name__ == '__main__':
+    app.run()
