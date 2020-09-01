ca2b6fc2791b6ba85cf8a2a1d2e30e82fbe968d1
khaldi505 <habetlbrika69@gmail.com>
2020-09-01 18:12:34 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# a script that starts a flask web application
+"""
+a script that starts a flask web application
+"""
 
 
 from flask import Flask
@@ -8,8 +10,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.url_map.strict_slashes = False
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ returns Hello hbnb!"""
     return "Hello HBNB!"
